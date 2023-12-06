f3a818580c543df13cc15b355546ac1fecdee36b
Rich Sharp <richpsharp@gmail.com>
2023-12-06 15:47:36 -0800

    fixing the pipeline

--- affiliation_classifier.py
+++ affiliation_classifier.py
@@ -22,26 +22,28 @@ def main():
     parser.add_argument('affiliation_pickle_list', help='path to affiliation pickle list')
     args = parser.parse_args()
 
-    classifier = pipeline(
-        "zero-shot-classification", model="facebook/bart-large-mnli")
 
     print('load affilation_list')
     affilation_set = set()
     for affiliation_pickle_file in glob.glob(args.affiliation_pickle_list):
-        with open(args.affiliation_pickle_list, 'r') as file:
+        with open(args.affiliation_pickle_list, 'r', encoding='utf-8') as file:
             for line in file:
                 affilation_set.add(line.rstrip().lstrip())
     affilation_list = list(affilation_set)
-    affilation_list = random.shuffle(affilation_list)
+    random.shuffle(affilation_list)
     print('load candidate_labels')
     with open('data/affiliation_tags.txt', 'r') as file:
-        candidate_labels = ','.join([
+        candidate_labels = ', '.join([
             v for v in file.read().split('\n')
             if len(v) > 0])
     print(candidate_labels)
 
-    with open('%s_classified%s' % os.path.splitext(args.affiliation_pickle_list)[0]) as file:
+    classifier = pipeline(
+        "zero-shot-classification", model="facebook/bart-large-mnli")
+
+    with open('%s_classified%s' % os.path.splitext(args.affiliation_pickle_list), 'w', encoding='utf-8') as file:
         for affiliation in affilation_list:
+            print(f'processing {affiliation}')
             file.write(f'{affiliation}\n')
             result = classifier(affiliation, candidate_labels)
             for label, score in zip(result['labels'], result['scores']):
