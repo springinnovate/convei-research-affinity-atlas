c8dde157f68b53c709d6bd36051e5d4aa12dff40
Rich Sharp <richpsharp@gmail.com>
2023-12-08 14:40:11 -0800

    abstract calssifier works

--- abstract_classifier.py
+++ abstract_classifier.py
@@ -5,6 +5,7 @@ import os
 import pickle
 import random
 import time
+import re
 
 from transformers import pipeline
 from flair.data import Sentence
@@ -24,22 +25,38 @@ LOGGER = logging.getLogger(__name__)
 
 def main():
     parser = argparse.ArgumentParser(description='Affiliation classifier')
-    parser.add_argument('affiliation_pickle_list', help='path to affiliation text list')
-    parser.add_argument('affiliation_tag_file', help='path to affiliation tags')
+    parser.add_argument('bib_file', help='path to bibliography list')
+    parser.add_argument('abstract_tag_file', help='path to abstract tags')
     parser.add_argument('--target_path', help='target classified table')
     args = parser.parse_args()
 
-
-    print('load affilation_list')
+    print('load bib_file')
     affilation_set = set()
-    for affiliation_pickle_file in glob.glob(args.affiliation_pickle_list):
-        with open(args.affiliation_pickle_list, 'r', encoding='utf-8') as file:
-            for line in file:
-                affilation_set.add(line.rstrip().lstrip())
-    affilation_list = list(affilation_set)
-    random.shuffle(affilation_list)
+    with open(args.bib_file, 'r', encoding='utf-8') as file:
+        affiliation_str = None
+        abstract_str = None
+        article_id = None
+        for line in file:
+            if '@ARTICLE{' in line:
+                if abstract_str is not None or affiliation_str is not None:
+                    print(f'WARNING: "{article_id}" had these were left over', abstract_str, affiliation_str)
+                article_id = re.search('@ARTICLE{(.*),', line).group(1)
+                affiliation_str = None
+                abstract_str = None
+                continue
+            elif 'abstract =' in line:
+                abstract_str = re.search('{(.*)}', line).group(1)
+            elif 'affiliations =' in line:
+                affiliation_str = re.search('{(.*)}', line).group(1)
+            if abstract_str and affiliation_str:
+                affilation_set.add((article_id, affiliation_str, abstract_str))
+                article_id = None
+                affiliation_str = None
+                abstract_str = None
+
     print('load candidate_labels')
-    with open('data/affiliation_tags.txt', 'r') as file:
+    print(len(affilation_set))
+    with open(args.abstract_tag_file, 'r') as file:
         candidate_labels = ', '.join([
             v for v in file.read().split('\n')
             if len(v) > 0])
@@ -47,46 +64,20 @@ def main():
 
     classifier = pipeline(
         "zero-shot-classification", model="facebook/bart-large-mnli")
-    token_tagger = SequenceTagger.load("flair/ner-english-ontonotes-large")
 
-    #tokenizer = AutoTokenizer.from_pretrained("dslim/bert-base-NER")
-    #tagger_model = AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER")
-
-    #token_tagger = pipeline("token-classification", model="dslim/bert-base-NER")
-
-    #token_tagger = pipeline("ner", model=tagger_model, tokenizer=tokenizer)
     target_path = args.target_path
     if target_path is None:
         target_path = '%s_classified%s' % os.path.splitext(args.affiliation_pickle_list)
 
     with open(target_path, 'w', encoding='utf-8') as file:
-        for affiliation in affilation_list:
+        for article_id, affiliation_str, abstract_str in affilation_set:
             start_time = time.time()
-            print(f'processing {affiliation}')
-            org_components = ''
-            tagged_result = token_tagger(affiliation)
-            print(tagged_result)
-            for entity in tagged_result:
-                print(entity)
-                if entity['entity_group'] in ['ORG', 'MIS']:
-                    org_components += f'{entity["word"]} '
+            print(f'processing {article_id}')
 
-            # tagged_affiliation = Sentence(affiliation)
-            # tagger.predict(tagged_affiliation)
-            # for entity in tagged_affiliation.get_spans('ner'):
-            #     if len(entity.text) > 5 and entity.get_label().value == 'ORG':
-            #         org_components += f'{entity.text} '
-            file.write(f'{affiliation}\n')
-            file.write(f'{org_components}\n')
-            if len(org_components) == 0:
-                org_components = affiliation
+            file.write(f'{article_id}\n{affiliation_str}\n{abstract_str}\n')
             result = classifier(
-                org_components, candidate_labels, multi_label=True)
-            if len(result) > 20:
-                continue
+                abstract_str, candidate_labels, multi_label=True)
             for label, score in zip(result['labels'], result['scores']):
-                if score < 0.8:
-                    break
                 file.write(f'{label}: {score}\n')
             file.write('\n')
             file.flush()
