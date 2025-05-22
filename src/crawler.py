795a646af1656da2572827c261163cec05580c7a
Richard P Sharp Jr. <richpsharp@gmail.com>
2025-05-22 15:00:30 -0700

    speed up queries a little

--- src/crawler.py
+++ src/crawler.py
@@ -1,3 +1,6 @@
+"""Webcrawler framework."""
+
+import time
 import asyncio
 import re
 from urllib.parse import urljoin, urlparse
@@ -8,8 +11,7 @@ from .database import SessionLocal
 from .models import URLContent
 
 
-async def fetch_page(url, browser):
-    page = await browser.new_page()
+async def fetch_page(url, page):
     await page.goto(url)
     content = await page.content()
     title = await page.title()
@@ -32,24 +34,37 @@ async def crawl_domain(start_url, max_pages, progress_dict, crawl_id):
     queue = asyncio.Queue()
     visited = set()
 
+    progress_dict[crawl_id] = {"analyzed": 0, "discovered": 1}
+
     await queue.put(start_url)
     visited.add(start_url)
 
-    progress_dict[crawl_id] = {"analyzed": 0, "discovered": 1}
-
     async with async_playwright() as pw:
         browser = await pw.chromium.launch()
+        page = await browser.new_page()
         while (
             not queue.empty()
             and progress_dict[crawl_id]["analyzed"] < max_pages
         ):
             url = await queue.get()
-            content, title = await fetch_page(url, browser)
 
-            page_record = URLContent(
-                url=url, content=content, title=title, analyzed=True
+            page_record = (
+                db.query(URLContent).filter(URLContent.url == url).first()
             )
-            db.merge(page_record)
+
+            if not page_record:
+                start = time.time()
+                content, title = await fetch_page(url, page)
+                print(f"took {time.time()-start:.2f}s to fetch {url}")
+                page_record = URLContent(
+                    url=url, content=content, title=title, analyzed=True
+                )
+                db.add(page_record)
+            else:
+                content = page_record.content
+                url = page_record.url
+                max_pages += 1  # we didn't search it, so do one more
+
             db.commit()
 
             links = await extract_links(content, url, domain)
